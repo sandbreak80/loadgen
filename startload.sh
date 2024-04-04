48502d6ee4bca04d16d0aa0f4537d383a4e8b8f8
Brad Stoner <bstoner@gmail.com>
2024-04-04 10:54:41 -0700

    Add files via upload

--- startload.sh
+++ startload.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# Filename: launch_loadgens.sh
+# Filename: startload.sh
 
 # This loop will start 10 instances of kk4.py using nohup and suppress their output
 for i in {1..10}; do
-   nohup python3 kk4.py >/dev/null 2>&1 &
-   echo "Instance $i of load generation started successfully"
+   nohup python3 /home/ubuntu/load/kk4.py >/dev/null 2>&1 &
+   echo "Instance $i of kk4.py load generation started successfully"
+done
+
+# Adding a small delay to ensure that the above processes have been initiated
+sleep 2
+
+# This loop will start 10 instances of test_checkout.py using nohup and suppress their output
+for i in {1..10}; do
+   nohup python3 t/home/ubuntu/load/est_checkout.py >/dev/null 2>&1 &
+   echo "Instance $i of test_checkout.py load generation started successfully"
 done
