bc0df7f78f6be2e9250d39f8f75bb1dc29c1e136
Brad Stoner <bstoner@gmail.com>
2024-04-04 11:12:12 -0700

    Add files via upload

--- install_loadgen.sh
+++ install_loadgen.sh
@@ -42,6 +42,7 @@ pip3 install webdriver-manager
 wget https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.86/linux64/chromedriver-linux64.zip -P /home/ubuntu/load
 
 # Unzip chromedriver to the specified directory
+mkdir /home/ubuntu/load/chromedriver-linux64
 unzip /home/ubuntu/load/chromedriver_linux64.zip -d /home/ubuntu/load/chromedriver-linux64/
 
 # Make chromedriver executable
